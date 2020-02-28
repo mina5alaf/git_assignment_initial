c6a5993793c7a1fc6d4f8def25490ba7a17525cb
ahmed <ahmed.focus19@gmail.com>
2020-02-28 23:57:54 +0200

    Adding CircleCircumference()

---
+++ src/CircleCircumference.java
@@ -0,0 +1,9 @@
+public class CircleCircumference implements ISubscriber {
+	@Override
+	public void notifySubscriber(String input) {
+        double radius=Double.parseDouble(input);
+		//System.out.println("Hello, I am really a simple subscriber and I am notified with " + input);
+        double circumference= Math.PI * 2*radius;
+        System.out.println( "The circumference of the circle is: "+circumference) ;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
+			new CircleCircumference()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
