f08a4686d2be38738f8f00b941da3b1fc0b4854a
NaglaEssam12 <59181709+NaglaEssam12@users.noreply.github.com>
2020-02-29 13:47:30 +0200

    Add Lucas Series Class that implement Function

---
+++ src/LucasSeriesSub.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class LucasSeriesSub implements ISubscriber {
+    @Override
+    public void notifySubscriber(String input) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello, I am a simple subscriber and I am notified with " + input);
+        int numOfTerms = Integer.parseInt(input);
+        ArrayList<Integer>lucesResult = new ArrayList<Integer>();
+        int temp1 = 2  , temp2 = 1;
+        int j = 0 , temp = 0;
+        if(numOfTerms == 1)
+        {
+            lucesResult.add(temp1);
+        }
+        else if (numOfTerms == 2)
+        {
+            lucesResult.add(temp1);
+            lucesResult.add(temp2);
+        }
+        else if (numOfTerms > 2)
+        {
+            lucesResult.add(temp1);
+            lucesResult.add(temp2);
+            for(int i = 2 ; i  < numOfTerms ; i++)
+            {
+                temp1 = lucesResult.get(j);
+                temp2 = lucesResult.get(++j);
+                temp = temp1 + temp2;
+                lucesResult.add(temp);
+            }
+        }
+        else
+        {
+            lucesResult.add(0);
+        }
+        System.out.println("The Series is : " + lucesResult);
+    }
+
+}
