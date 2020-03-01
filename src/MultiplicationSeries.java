a4eb2139853269becd0ad5e1c9c4771108a4e74c
ayaamr11 <36492846+ayaamr11@users.noreply.github.com>
2020-03-01 20:59:20 +0200

    Update MultiplicationSeries.java

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -1,20 +1,17 @@
-
 public class MultiplicationSeries implements ISubscriber {
 	public void notifySubscriber(String input){	
 		// TODO Auto-generated method stub
-		System.out.print(" the multiplication series of  " + input);
-	
+		
 		int n =Integer.parseInt(input);
 		int result = n;
-		System.out.println(n);
+		
 		
 		while(n>1) {
 			n-=1;
 			result =result *n;
-			System.out.println(n);
-			System.out.println(result);
+			
 
 		}
-		System.out.println(" is " + result );
+		System.out.println("the multiplication series of  " + input +" is " + result );
 	}
 }
